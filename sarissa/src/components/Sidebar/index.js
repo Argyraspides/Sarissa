787846a4249e218689f87341707f89697c29d5a5
Argyraspides <joshivinayak441@gmail.com>
2023-11-19 02:37:49 +1300

    GPQ-N7

---
+++ sarissa/src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+import {Link} from 'react-router-dom'
+const Sidebar = () => {
+    <div>
+        <Link className='logo' to='/'>
+            <img src={null}></img>
+        </Link>
+    </div>
+}
+
+export default Sidebar
