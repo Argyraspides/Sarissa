c0870c3d08a7ae521008d8799e6ec815cc39e9cc
Argyraspides <joshivinayak441@gmail.com>
2023-11-20 15:20:50 +1300

    Fixed some dark reader plugins disabling background rendering

--- sarissa/src/components/Background/index.js
+++ sarissa/src/components/Background/index.js
@@ -1,11 +1,11 @@
 import './index.scss';
-import TwinklingBackground from '../../assets/video/twinkling.mp4';
+import TwinklingBackground from '../../assets/video/twinkling.webm';
 
 const Background = () => {
   return (
     <div className='twinkling-backgrounds'>
         <video autoPlay loop muted playbackRate={0.1}>
-        <source src={TwinklingBackground} type='video/mp4' />
+        <source src={TwinklingBackground} type='video/webm' />
         </video>  
     </div>
   );
