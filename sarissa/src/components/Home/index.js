9d10af9362cc79810cb8da672f33503996eeb62c
Argyraspides <joshivinayak441@gmail.com>
2023-11-20 13:58:20 +1300

    Added PE cover

--- sarissa/src/components/Home/index.js
+++ sarissa/src/components/Home/index.js
@@ -10,13 +10,19 @@ const Home = () => {
           <br></br>
           <span><Typewriter text = "Welcome to my ship." delay={50}/></span>
         </h1>
-        <h4>
+        <h2>
             <span><Typewriter text = 'Passionate, tenacious, and always willing to put "we" over "me".' delay={50}/></span>
             <br></br>
             <span><Typewriter text = 'I am currently a final year software engineering student at the University of Auckland' delay={50}/></span>
             <br></br>
             <span><Typewriter text = 'with a love all things software, having a special interest in modelling the real world.' delay={50}/></span>
-        </h4>
+            <br></br>
+        </h2>
+
+        <h3>
+            <span><Typewriter text = 'Check out my mini-projects right here!' delay={50}/></span>
+        </h3>
+
       </div>
     );
   };
