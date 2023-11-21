87a86a17a8cf817c80093a7756d07a6174552b8d
Argyraspides <joshivinayak441@gmail.com>
2023-11-21 17:29:56 +1300

    Added fade to about logo

--- sarissa/src/components/About/index.js
+++ sarissa/src/components/About/index.js
@@ -2,11 +2,13 @@ import './index.scss'
 import Background from '../Background'
 import Sidebar from '../Sidebar'
 import Logo from '../../assets/images/initials_logo_purple_b1.png'
+import Typewriter from '../Home/Typewriter'
 
 
 
 const About = () =>
 {
+    const txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     return (
         <div>
             <Background/>
@@ -15,13 +17,9 @@ const About = () =>
                 <img src={Logo} className='logo'></img>
                 <div className='desc-container'>
                     <h1 className='desc'>
-                        Deez nuts
+                        <Typewriter text ='Deez nuts' delay={30}/>
                         <h2>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <Typewriter text={txt} delay={30}/>
                         </h2>
                     </h1>
 
