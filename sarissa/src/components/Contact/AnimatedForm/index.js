0852ae7bc6f9b7958df78193f304e6a04ef77e03
Argyraspides <joshivinayak441@gmail.com>
2023-11-24 03:07:09 +1300

    Reinstantiated form sender. Desktop front-end complete!

---
+++ sarissa/src/components/Contact/AnimatedForm/index.js
@@ -0,0 +1,54 @@
+import { useRef } from 'react'
+import './index.scss'
+import emailjs from "@emailjs/browser"
+const AnimatedForm = () => {
+
+    const refForm = useRef()
+
+
+    const sendEmail = (e) =>
+    {
+        e.preventDefault()
+
+        emailjs.sendForm(
+            'service_lf6emqb',
+            'template_j34t4mf',
+            refForm.current,
+            'RiYEg9RxIZ3E_xr3y'
+        ).then (
+            () => {
+                alert('Message sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send message. Please try again.')
+            }
+        )
+    }
+
+    return (
+        <div className='contact-form'>
+            <form ref={refForm} onSubmit={sendEmail}>
+            <ul>
+                <li className='half'>
+                    <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className='half'>
+                    <input type="email" name="email" placeholder="Email" required />
+                </li>
+                <li>
+                    <input placeholder="Subject" type="text" name="subject" required />
+                </li>
+                <li>
+                    <textarea placeholder="Message" name="Message" required></textarea>
+                </li>
+                <li>
+                    <input type="submit" className='flat-button' value="send"></input>
+                </li>
+            </ul>
+            </form>
+        </div>
+    )
+}
+
+export default AnimatedForm
