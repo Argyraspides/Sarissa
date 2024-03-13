0abfee15859927991e7178d925bcbd292e5e2956
Argyraspides <joshivinayak441@gmail.com>
2024-03-13 13:57:40 +1300

    Added analytics

--- sarissa/src/App.js
+++ sarissa/src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Projects from './components/Projects';
 import About from './components/About'
 import Contact from './components/Contact';
@@ -7,21 +7,22 @@ import Contact from './components/Contact';
 import Background from './components/Background';
 import PhysicsEngine from './components/Physics_Engine';
 import FluidSimulator from './components/Fluid_Simulator'
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
 
-  
+
   return (
-   <>
-   <Background/>
-    <Routes>
-      <Route path = "/" element={<About/>} />
-      <Route path = "/projects" element={<Projects/>} />
-      <Route path = "/contact" element={<Contact/>} />
-      <Route path="/telos" element={<PhysicsEngine />} />
-      <Route path="/rheos" element={<FluidSimulator />} />
-    </Routes>
-   </> 
+    <>
+      <Background />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/telos" element={<PhysicsEngine />} />
+        <Route path="/rheos" element={<FluidSimulator />} />
+      </Routes>
+      <Analytics></Analytics>
+    </>
   )
 }
 
